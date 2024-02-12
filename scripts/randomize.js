c07420b2e9d016b9bfc9bf677458e07864dffd2b
Jovinil <134116459+Jovinil@users.noreply.github.com>
2024-02-12 19:06:49 +0800

    Add files via upload

--- scripts/randomize.js
+++ scripts/randomize.js
@@ -1,18 +1,9 @@
-let images = ["img1.jpeg", "img2.jpg", "img3.jpeg"]; 
-
 let no = document.querySelector(".random");
 
 function randomize(){
     let randomX = Math.floor(Math.random() * 10 + 1);
     let randomY = Math.floor(Math.random() * -15 + 1);
     absolute(randomX, randomY);
-
-    let randomImg = Math.floor(Math.random() * 4);
-    let imgContainer = document.querySelector(".img-container");
-    imgContainer.innerHTML = '';
-    let img = document.createElement("img");
-    img.src = `/images/${images[randomImg]}`;
-    imgContainer.append(img);
 }
 
 function absolute(x, y){
